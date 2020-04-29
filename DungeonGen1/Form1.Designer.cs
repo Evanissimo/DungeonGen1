245050368916d1a91e19a0e940047e7e9ac0b9eb
Evanissimo <ejpanek@gmail.com>
2020-04-28 23:22:22 -0700

    Add project files.

---
+++ DungeonGen1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DungeonGen1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(332, 180);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(75, 23);
+            this.generate.TabIndex = 0;
+            this.generate.Text = "Generate";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.generate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generate;
+    }
+}
+
---
+++ DungeonGen1/magicItems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGen1
+{
+    class magicItems : placeableEntity
+    {
+        public override void decidePlace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void place(int x, int y, char marker)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
